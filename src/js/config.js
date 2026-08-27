86aaac03f7492c463de3bd302f17016ca124cf14
peeraphatsrikaew50-afk <peeraphatsrikaew50@gmail.com>
2026-08-27 20:01:31 +0700

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,11 +1,3 @@
 const CONFIG = {
-  // Web App URL จาก Google Apps Script
-  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbxMv5yT3L8CAMMOC0-dvrzANhR5p5h7zj7rUsN3KwvAFKWgVgPrdfs44it49FP_flBX/exec',
-
-  ROLES: {
-    GUEST: 'Guest',
-    MEMBER: 'Member',
-    ARTIST: 'Artist',
-    ADMIN: 'Admin'
-  }
+  API_BASE_URL: "https://script.google.com/macros/s/AKfycbxMv5yT3L8CAMMOC0-dvrzANhR5p5h7zj7rUsN3KwvAFKWgVgPrdfs44it49FP_flBX/exec"
 };
